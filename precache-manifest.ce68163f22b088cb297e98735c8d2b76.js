60d21b42927edb0e7c18c3ca6879056589f826d2
Divindra Ramai <daveramai@hotmail.com>
2020-07-07 23:10:51 -0400

    Updates

---
+++ precache-manifest.ce68163f22b088cb297e98735c8d2b76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770a1cde9e4d36e1a79fad7844ca1527",
+    "url": "/booklist/index.html"
+  },
+  {
+    "revision": "9b4538b31211fc062abc",
+    "url": "/booklist/static/css/main.13edc697.chunk.css"
+  },
+  {
+    "revision": "81ccc959c6c7d25ad021",
+    "url": "/booklist/static/js/2.a5b155f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/booklist/static/js/2.a5b155f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4538b31211fc062abc",
+    "url": "/booklist/static/js/main.0566dfc1.chunk.js"
+  },
+  {
+    "revision": "f55020a32a73c9d08978",
+    "url": "/booklist/static/js/runtime-main.a768f342.js"
+  }
+]);
